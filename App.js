e804f256a4e92b65a6f69f731164d54dbd29d360
rahul panjiyar <raul.panjiyar@gmail.com>
2018-05-09 15:18:50 +0545

    second commit

---
+++ App.js
@@ -0,0 +1,13 @@
+'use strict';
+import React from 'react';
+import Main from './App/Component/Main'
+export default class App extends React.Component
+{
+  render(){
+    return(
+    <Main/>
+  );
+  }
+}
+
+
