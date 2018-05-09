dfb3fc51d862da320a5d59bf216ba55f901595b2
deadrules <39c49ae0a77bf56e19d80e3754f321afbfb6232f>
2018-05-09 15:29:33 +0545

    third commit

---
+++ App/Component/Main.js
@@ -0,0 +1,131 @@
+'use strict';
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    ScrollView,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+} from 'react-native';
+
+import Note from './Note';
+
+export default class Main extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            noteArray: [],
+            noteText: '',
+        };
+    }
+    render() {
+        let notes = this.state.noteArray.map((val, key) => {
+            return <Note key={key} keyval={key} val={val}
+                deleteMethod={() => this.deleteNote(key)} />
+        });
+
+        return (
+
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.headerText}>-Notes-</Text>
+                </View>
+                <ScrollView
+                
+                    style={styles.scrollcontainer}>Ï
+                    {notes}
+                </ScrollView>
+                <View style={styles.footer}>
+                    <TextInput style={styles.footerText}
+                        placeholder=' Add Note'
+                        onChangeText={(noteText) => this.setState({ noteText })}
+                        value={this.state.noteText}
+                        placeholderTextColor='white'
+                        underlineColorAndroid='transparent'></TextInput>
+                </View>
+                <TouchableOpacity onPress={this.addNote.bind(this)}
+                    style={styles.addbutton}>
+                    <Text style={styles.addbuttonText}>+</Text></TouchableOpacity>
+
+
+            </View>
+
+        );
+    }
+    addNote() {
+        if (this.state.noteText) {
+            var d = new Date();
+            this.state.noteArray.push({
+                'date': d.getFullYear() +
+                    "/" + (d.getMonth() + 1) +
+                    "/" + d.getDate(),
+                'note': this.state.noteText
+            });
+            this.setState({ noteArray: this.state.noteArray });
+            this.setState({ noteText: '' });
+        }
+    }
+
+    deleteNote(key) {
+        this.state.noteArray.splice(key, 1);
+        this.setState({ noteArray: this.state.noteArray });
+        alert('Note Deleted')
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        backgroundColor: '#98cd01',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomWidth: 10,
+        borderBottomColor: '#ddd',
+    },
+    headerText: {
+        color: 'white',
+        fontSize: 18,
+        padding: 26,
+    },
+    scrollcontainer: {
+        flex: 1.5,
+        marginBottom: 100,
+    },
+    footer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 10,
+    },
+    footerText: {
+        alignSelf: 'stretch',
+        color: '#fff',
+        padding: 20,
+        backgroundColor: '#252525',
+        borderTopWidth: 2,
+        borderTopColor: '#ededed',
+    },
+    addbutton: {
+        position: 'absolute',
+        zIndex: 11,
+        right: 20,
+        bottom: 90,
+        backgroundColor: '#98cd01',
+        width: 60,
+        height: 60,
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 8,
+    },
+    addbuttonText: {
+        color: '#fff',
+        fontSize: 24,
+    },
+
+});
---
+++ App/Component/SearchPage.js
@@ -0,0 +1,25 @@
+'use strict';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+
+export default class SearchPage extends React.Component{
+  render(){
+    return(
+      <View style = {styles.container}>
+        
+        
+      </View>
+
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container : {
+   
+  },
+});
